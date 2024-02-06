fb9afe0f08d103aebb8eb668a59c8b2f8bb62767
xtidozy <xtidozy@outlook.com>
2024-02-06 17:19:25 +0800

    '---'

---
+++ min/v.js
@@ -0,0 +1,30 @@
+if (neatSrc) neatSrc.push({
+    domainUrl: "https://sia.dozy.eu.org",
+    backgrounds: [
+    ],
+    music: [
+        {id: "Foreign_Friends", name: sc.ra},
+        {id: "japan1", name: sc.sp + "#ありがとう", author: "KOKIA"},
+{id: "Ngau_Hung", name: sc.ra, author: "Hoaprox"},
+{id: "Guitar_Type", author: "ET Soul"},
+{id: "Alleyways", name: sc.ra, author: "NightCore"},
+{id: "Star_Track", author: "NightCore"},
+{id: "Bgm_A_MC_Server", name: sc.ep},
+{id: "Deep_Blue", name: sc.ra, author: "William Black"},
+        {id: "SUECO"},
+        {id: "Duvet", name: sc.sp, author: "Boa"},
+        {id: "Duvet_Pure", name: sc.ep + "#Duvet" + "(纯)".inc("w"), author: "Boa"},
+        {id: "Miraie", name: sc.ep},
+        {id: "Save_Me", name: sc.ms},
+        {id: "Leaving_Home", name: sc.ep, author: "Park Bird"},
+{id: "Experiment_626", name: sc.ra, author: "WORKER"},
+{id: "Now_Or_Never", name: sc.ep, author: "Valentin".ank(sc.ra)},
+{id: "Bgm_Dungeon", name: sc.ra},
+{id: "Bgm_Fast_Sky", name: sc.sp},
+{id: "Cdong4dong4", name: sc.sp + "#動く、動く", author: "久保ユリカ&水瀬いのり"},
+{id: "SO_TIRED", name: sc.ra, author: "Avery"},
+        {id: "Czang4_Pure", name: sc.ra + "#葬" + "(纯)".inc("w"), author: "Cover:18"},
+        {id: "Cxin1pai1shu4", name: sc.ep + "#心拍数" + "(口琴)".inc("w"), author: "Hodge"},
+        {id: "Bgm_Cchou1jiang3xi4tong3", name: sc.sp + "#Bgm" + cs.s + "抽奖系统".inc("a")}
+    ]
+});
